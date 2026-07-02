579f15550dfe830915a7328b711ac5403be6a10d
Zaara <zaara@example.com>
2026-07-02 21:02:54 +0530

    Add admin auto refresh

---
+++ qr.js
@@ -0,0 +1,9 @@
+const QRCode = require("qrcode");
+const url = "https://delivery-app-ders.onrender.com/index.html";
+QRCode.toFile("qr.png", url, (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log("QR saved as qr.png");
+});
